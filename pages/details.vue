f07d48f104111027b49fb08fe5ed82e9737f616f
mysterymosim <okimmosimiracle@gmail.com>
2020-11-27 12:32:34 +0000

    minor modifications

--- pages/details.vue
+++ pages/details.vue
@@ -5,16 +5,16 @@ const name = "DetailsStarterPage";
 
 const components = { DetailsStarter };
 
-async function asyncData({ $axios }) {
-  // check ../nuxt.config.js for where this comes from
-  const person = await $axios.$get("/api/person");
-  return { person };
-}
+// async function asyncData({ $axios }) {
+//   // check ../nuxt.config.js for where this comes from
+//   const person = await $axios.$get("/api/person");
+//   return { person };
+// }
 
 export default {
   name,
-  components,
-  asyncData
+  components
+  // asyncData
 };
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,23 @@
 <script>
-import FormStarter from "~/screens/FormStarter.vue";
+// import FormStarter from "~/screens/FormStarter.vue";
 
 const name = "FormStarterPage";
 
-const components = { FormStarter };
+// const components = { FormStarter };
 
-async function asyncData({ $axios }) {
-  // check ../nuxt.config.js for where this comes from
-  const person = await $axios.$get("/api/person");
-  return { person };
-}
+// async function asyncData({ $axios }) {
+//   // check ../nuxt.config.js for where this comes from
+//   const person = await $axios.$get("/api/person");
+//   return { person };
+// }
 
 export default {
-  name,
-  components,
-  asyncData
+  name
+  // components
+  // asyncData
 };
 </script>
 
 <template>
-  <FormStarter :person="person" />
+  <!-- <FormStarter :person="person" /> -->
 </template>
