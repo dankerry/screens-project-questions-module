db5c001070ecc02e35c73fc9dddcca59708a8453
mysterymosim <okimmosimiracle@gmail.com>
2020-11-27 11:57:54 +0000

    generated static pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,13 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: "static",
-  
-  // router: {
-  //   base: '/details/'
-  // },
 
+  generate: {
+    routes: [
+      "/details"
+    ]
+  },
+  
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
